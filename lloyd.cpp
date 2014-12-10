e5dbe878d5e0548ec66cd027220e76e6923d3baf
Pratik Sajnani <code.jtr@gmail.com>
2014-12-10 00:15:57 -0500

    Implemented lloyd puzzle

--- lloyd.cpp
+++ lloyd.cpp
@@ -13,23 +13,58 @@ int main(int argc, char ** argv)
 		cout<<"Usage 2 : ./lloyd - -"<<endl;
 		return 1;	
 	}
-	
+
 	string inputFile = argv[1];
 	string outputFile = argv[2];
 
-	
+
 	//parse initial file to get output
-		
+
 	//initialize the water puzzle object
 	vector<vector<int> > initial;
 	vector<vector<int> > goal;
+
+	vector<int> row1;
+	vector<int> row2;
+	vector<int> row2Other;
+	vector<int> row3;
+	vector<int> row3Other;
+
+	row1.push_back(1);
+	row1.push_back(2);
+	row1.push_back(3);
+
+	row2.push_back(4);
+	row2.push_back(5);
+	row2.push_back(6);
+
+	row3.push_back(7);
+	row3.push_back(8);
+	row3.push_back(0);
+
+	row2Other.push_back(4);
+	row2Other.push_back(5);
+	row2Other.push_back(0);
+
+	row3Other.push_back(7);
+	row3Other.push_back(8);
+	row3Other.push_back(6);
+
+	initial.push_back(row1);
+	initial.push_back(row2Other);
+	initial.push_back(row3Other);
+
+	goal.push_back(row1);
+	goal.push_back(row2);
+	goal.push_back(row3);
+
 	MyLloyd lloyd(initial, goal);
 
 	Solver< vector<vector<int> >, vector<vector<int> > > solver(lloyd);
-	
+
 	//call the solve function to get a solution
 	cout<<"Solving lloyd puzzle"<<endl;
 	solver.solve();
-	
+
 	return 0;
 }
--- mylloyd.cpp
+++ mylloyd.cpp
@@ -5,7 +5,7 @@
 
 using namespace std;
 
-MyLloyd::MyLloyd(vector<vector<int> > goal, vector<vector<int> > initial): Puzzle(initial,goal)
+MyLloyd::MyLloyd(vector<vector<int> > initial, vector<vector<int> > goal): Puzzle(initial,goal)
 {
 }
 
@@ -20,14 +20,68 @@ bool MyLloyd::isGoal(vector<vector<int> > current) const
 		return false;
 	}
 }
-vector<vector<vector<int> > > MyLloyd::next(vector<vector<int> > matrix)
+vector<vector<vector<int> > > MyLloyd::next(const vector<vector<int> > current)
 {	
 	vector<vector<vector<int> > > nextConfigList;
 
-	for(vector<vector<int> >::const_iterator row = matrix.begin(); row!=matrix.end(); ++row)
+	vector<vector<int> > matrix = current; 
+	int temp = -1;
+	vector<vector<int> >::iterator tempRow;
+	vector<int>::iterator tempCol;
+	for(vector<vector<int> >::iterator row = matrix.begin(); row!=matrix.end(); ++row)
 	{
-		for(vector<int>::const_iterator col = row->begin(); col!=row->end(); ++col)
-		//TODO - get next configs	
+		for(vector<int>::iterator col = row->begin(); col!=row->end(); ++col)
+			if(*col==0)
+			{
+				//move right
+				if((col+1) != row->end())
+				{	
+					tempCol = col+1;
+					temp = *tempCol;
+					*tempCol = *col;
+					*col = temp;	
+					nextConfigList.push_back(matrix);
+					matrix = current;
+					temp = -1;
+				}
+				//move left
+				if(col != row->begin())
+				{
+					tempCol = col-1;
+					temp = *tempCol;
+					*tempCol = *col;
+					*col = temp;
+					nextConfigList.push_back(matrix);
+					matrix = current;
+					temp = -1;
+				}
+				//move down
+				if((row+1)!=matrix.end())
+				{
+					tempRow = row+1;
+					tempCol = (tempRow->begin() + (col - row->begin()));
+					temp = *col;
+					*col = *tempCol;
+					*tempCol = temp;
+					nextConfigList.push_back(matrix);
+					matrix = current;
+					temp = -1;
+				}
+				//move up
+				if(row!=matrix.begin())
+				{
+					tempRow = row-1;
+					tempCol = (tempRow->begin() + (col - row->begin()));
+					temp = *col;
+					*col = *tempCol;
+					*tempCol = temp;
+					nextConfigList.push_back(matrix);
+					matrix = current;
+
+					temp = -1;
+
+				}
+			}
 	}
 
 	return nextConfigList;
@@ -45,7 +99,8 @@ string MyLloyd::print(const vector<vector<int> > &matrix) const
 	{
 		for(vector<int>::const_iterator col = row->begin(); col!=row->end(); ++col) 
 			stream<<*col<<",";
+		stream<<endl;
 	}
-	stream<<"\n";
+	
 	return stream.str();
 }
